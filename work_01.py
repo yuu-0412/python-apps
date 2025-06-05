e519db6b3c0749cb5e073dbb13dbdca47d809a12
yuu-0412 <yuu.oxo@icloud.com>
2025-06-05 11:38:56 +0900

    a

---
+++ work_01.py
@@ -0,0 +1,31 @@
+import random
+
+random_n = random.randint(1,100)
+#random_n = 7
+for i in range(5):
+    cnt = cnt + 1
+    input_line = int(input("1~100までの数"))
+    print("入力された値",input_line)
+    if random_n == int(input_line):
+        print("正解")
+        result = True
+        break
+    else:
+        print("不正解")
+        if input_line < answer:
+            if answer - input_line > 10:
+                print("もっと大きい")
+            else:
+                print("もっと小さい数字ですが、だいぶ近い")   
+
+while True:
+    result = run_game()                
+    
+    if result:
+        print("ゲームに勝ちました！あなたは{cnt}回目で正解しました") 
+    else:
+        print("ゲームに負けました。正解は{answer}")   
+
+    ans = input("もう一度やりますか？(yes/no):")
+                                     
+    print("正解の値",random_n)
