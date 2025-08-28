a72587ac29fb5f64ea9c51da90210dd7f46d570a
yuu-0412 <yuu.oxo@icloud.com>
2025-08-28 12:16:47 +0900

    work1_01.py

--- work_01.py
+++ work_01.py
@@ -1,37 +1,61 @@
 import random
+import mysql.connector
 
-answer = random.randint(1,100)
-#random_n = 7
-for i in range(5):
-    cnt = cnt + 1
-    input_line = int(input("1~100までの数"))
-    print("入力された値",input_line)
-    if answer == int(input_line):
-        print("正解")
-        result = True
-        break
-    else:
-        print("不正解")
-        if input_line < answer:
-            if answer - input_line > 10:
-                print("もっと大きい")
-            else:
-                print("もっと小さい数字ですが、だいぶ近い")   
+# MySQLに接続
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",         # あなたのユーザー名
+    password="yuka0227", # rootのパスワード
+    database="work1db"   # データベース名
+)
+cur = conn.cursor()
 
 while True:
-    result = ()              
-    
+    answer = random.randint(1, 100)
+    result = False
+    cnt = 0
+    guesses = []
+
+    for i in range(5):
+        cnt += 1
+        input_line = int(input("1から100の数字を入力してください: "))
+        guesses.append(input_line)
+
+        if input_line == answer:
+            print("正解です！")
+            result = True
+            break
+        else:
+            print("不正解です。")
+            if input_line < answer:
+                if answer - input_line > 10:
+                    print("10以上大きいです")
+                else:
+                    print("もっと大きい数字ですが、だいぶ近いです")
+            else:
+                if input_line - answer > 10:
+                    print("10以上小さいです")
+                else:
+                    print("もっと小さい数字ですが、だいぶ近いです")
+
     if result:
-        print("ゲームに勝ちました！あなたは{cnt}回目で正解しました") 
+        print(f"ゲームに勝ちました！ あなたは{cnt}回目で正解しました。")
+        game_result = "win"
     else:
-        print("ゲームに負けました。正解は{answer}")   
+        print(f"ゲームに負けました。正解は{answer}でした。")
+        game_result = "lose"
 
-    ans = input("もう一度やりますか？(yes/no):")
+    # データベースに保存
+    cur.execute('''
+        INSERT INTO work1_results (answer, attempts, result, guesses)
+        VALUES (%s, %s, %s, %s)
+    ''', (answer, cnt, game_result, ",".join(map(str, guesses))))
+    conn.commit()
 
-    if ans == "yes":
-        print("お疲れ")
+    ans = input("もう一度やりますか？(yes/no): ")
+    if ans != "yes":
+        print("おつかれさまでした！")
         break
 
-
-    print("正解の値",answer)
-
+cur.close()
+conn.close()
